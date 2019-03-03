0c26cf5c6d77cc7ecc45786d2360bfb27969a911
Galen <galencasstevens@gmail.com>
2019-03-03 09:24:30 -0500

    finished 1.2 first solution

--- Algorithms.java
+++ Algorithms.java
@@ -35,4 +35,40 @@ public class Algorithms {
 		
 		return true;
 	}
+	
+	
+	// Given two strings, write a method to decide if one is a permutation of the other.
+	static public boolean areStringsPermutation(String string1, String string2) {
+		if(string1.length() != string2.length()) return false;
+		
+		HashMap<Character, Integer> map1 = new HashMap<>();
+		HashMap<Character, Integer> map2 = new HashMap<>();
+		Character charInString1;
+		Character charInString2;
+		
+		for(int i = 0; i < string1.length(); i++) {
+			charInString1 = string1.charAt(i);
+			charInString2 = string2.charAt(i);
+			
+			if(!map1.containsKey(charInString1)) {
+				map1.put(charInString1, 0);
+			} else {
+				map1.put(charInString1, map1.get(charInString1) + 1);
+			}
+			
+			if(!map2.containsKey(charInString2)) {
+				map2.put(charInString2, 0);
+			} else {
+				map1.put(charInString2, map2.get(charInString2) + 1);
+			}
+			
+			map2.put(charInString2, 0);
+		}
+		
+		for(Character c: map1.keySet()) {
+			if(map1.get(c) != map2.get(c)) return false;
+		}
+		
+		return true;
+	}
 }
--- Driver.java
+++ Driver.java
@@ -3,17 +3,17 @@ import java.util.HashMap;
 public class Driver {
 	public static void main(String[] args) {
 		Algorithms a = new Algorithms();
-		String sample1 = "roses",
-			   sample2 = "red",
-			   sample3 = "teen",
-			   sample4 = "rocket",
-			   sample5 = "moon";
+		String[] sample1 = new String[]{"top", "pot"};
+		String[] sample2 = new String[]{"thot", "hotte"};
+		String[] sample3 = new String[]{"dear", "read"};
+		String[] sample4 = new String[]{"code", "core"};
+		String[] sample5 = new String[]{"god", "dog"};
 		
-		System.out.println(a.isStringUnique2(sample1));
-		System.out.println(a.isStringUnique2(sample2));
-		System.out.println(a.isStringUnique2(sample3));
-		System.out.println(a.isStringUnique2(sample4));
-		System.out.println(a.isStringUnique2(sample5));
+		System.out.println(a.areStringsPermutation(sample1[0], sample1[1]));
+		System.out.println(a.areStringsPermutation(sample2[0], sample2[1]));
+		System.out.println(a.areStringsPermutation(sample3[0], sample3[1]));
+		System.out.println(a.areStringsPermutation(sample4[0], sample4[1]));
+		System.out.println(a.areStringsPermutation(sample5[0], sample5[1]));
 	}
 }
 
